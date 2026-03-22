3c81d00a3f06033b56d5de73dd133a7960336ef8
Luan Lisboa Medeiros <109869151+medeirosluan@users.noreply.github.com>
2026-03-22 20:25:19 -0300

    atualização

--- script.js
+++ script.js
@@ -6,82 +6,131 @@ const secondValor = document.querySelector('.second-valor')
 let resultado = null
 
 
-function getOperation(){
+function getOperation() {
 
     let valorSelect = operation.value
     let getFirstValor = Number(firstValor.value)
     let getSecondValor = Number(secondValor.value)
 
+    //verifica se valor do input é igual a SUM
     if (valorSelect === 'sum') {
-        return sum(getFirstValor,getSecondValor)
-    }
-    
-    else if (valorSelect === 'subtract') {
-        return subtract(getFirstValor,getSecondValor)
-    }
-    
-    else if (valorSelect === 'multipli') {
-        return multipli(getFirstValor,getSecondValor)
-    }
-    
-    else if (valorSelect === 'divide') {
-        return divide(getFirstValor,getSecondValor)
+        return sum(getFirstValor, getSecondValor)
     }
 
+    //verifica se valor do input é igual a subtract
+    else if (valorSelect === 'subtract') {
+        return subtract(getFirstValor, getSecondValor)
+    }
+
+    //verifica se valor do input é igual a multipli
+    else if (valorSelect === 'multipli') {
+        return multipli(getFirstValor, getSecondValor)
+    }
+
+    //verifica se valor do input é igual a divide
+    else if (valorSelect === 'divide') {
+        return divide(getFirstValor, getSecondValor)
+    }
+
+    //verifica se valor selecionado e igual a raiz
     else if (valorSelect === 'raiz') {
         return raiz(getFirstValor)
     }
 
+    else if (valorSelect === 'even') {
+        return parImpar(getFirstValor)
+    }
+
 }
 
-function sum(getFirstValor,getSecondValor) {
+
+function sum(getFirstValor, getSecondValor) {
     resultado = getFirstValor + getSecondValor
     return msg.innerHTML = `${resultado}`
 }
 
-function subtract(getFirstValor,getSecondValor) {
+
+function subtract(getFirstValor, getSecondValor) {
     resultado = getFirstValor - getSecondValor
-   return msg.innerHTML = `${resultado}`
+    return msg.innerHTML = `${resultado}`
 }
 
-function multipli(getFirstValor,getSecondValor) {
+
+function multipli(getFirstValor, getSecondValor) {
     resultado = getFirstValor * getSecondValor
-   return msg.innerHTML = `${resultado}`
+    return msg.innerHTML = `${resultado}`
 }
 
-function divide(getFirstValor,getSecondValor) {
+
+function divide(getFirstValor, getSecondValor) {
     resultado = getFirstValor / getSecondValor
-   return msg.innerHTML = `${resultado}`
+    return msg.innerHTML = `${resultado}`
 }
+
 
 function raiz(getFirstValor) {
     let numberRaiz = Math.sqrt(getFirstValor)
     return msg.innerHTML = `${numberRaiz.toFixed(2)}`
 }
 
-function quadrada(){
-   
-    if (operation.value === 'raiz') {
+
+function parImpar(getFirstValor) {
+
+    if (getFirstValor % 2 === 0) {
+        return msg.innerHTML = `${getFirstValor}: PAR`
+    }
+
+    return msg.innerHTML = `${getFirstValor}: IMPAR`
+}
+
+
+function evenOdd() {
+
+    if (operation.value === 'even') {
         secondValor.classList.add('oculta')
-        document.querySelector('h1').textContent = 'Raiz Quadrada'
-        btn.textContent = 'Descobrir Raiz'
-    
+        document.querySelector('h1').textContent = 'Impar OU Par'
+        btn.textContent = 'PAR/IMPAR'
+        firstValor.placeholder = 'Par ou Impar';
+
     } else {
         secondValor.classList.remove('oculta')
         document.querySelector('h1').textContent = 'Calculadora'
         btn.textContent = 'Calcular'
-    
+        firstValor.placeholder = 'Digite o 1º numero';
+    }
+
+}
+
+function quadrada() {
+
+    if (operation.value === 'raiz') {
+        secondValor.classList.add('oculta')
+        document.querySelector('h1').textContent = 'Raiz Quadrada'
+        btn.textContent = 'Descobrir Raiz'
+        firstValor.placeholder = 'Raiz do numero digitado é...';
+
+    } else {
+        secondValor.classList.remove('oculta')
+        document.querySelector('h1').textContent = 'Calculadora'
+        btn.textContent = 'Calcular'
+        firstValor.placeholder = 'Digite o 1º numero';
     }
 }
 
 
-operation.addEventListener('change',()=>{
+operation.addEventListener('change', () => {
     quadrada()
+    evenOdd()
 })
 
-btn.addEventListener('click',()=>{
+
+btn.addEventListener('click', () => {
     getOperation()
+    firstValor.value = ''
+    secondValor.value = ''
 })
+
+
 
 
 
